7ed7c41f546161ce90ea36c784711e2c48679c05
solominh <hoangminhftu2@gmail.com>
2017-01-10 00:07:31 +0700

    Save

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ from app import main
 
 
 test_url = 'https://www.facebook.com/linhtam.ha.311/posts/113868739119026'
-main(test_url)
+test_url2 = 'https://www.facebook.com/permalink.php?story_fbid=117248922113572&id=100014853942578'
+main(test_url2)
